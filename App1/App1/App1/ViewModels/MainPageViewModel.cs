09c406a3f7669c9addd9b58291b8d2b4893e3996
jnuno420 <61151080+jnuno420@users.noreply.github.com>
2020-02-26 12:35:47 +0000

    Mudanças ao .vpp e acrescentei o menu de login do vs

---
+++ App1/App1/App1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class MainPageViewModel : ContentPage
+    {
+        Button b;
+        public MainPageViewModel()
+        {
+            
+            FelixCommand = new Command(() =>
+            {
+                b = new Button
+                {
+                    Text = "Felixxx",
+                    TextColor = Color.White,
+                    BackgroundColor = Color.Blue,
+                    HorizontalOptions = LayoutOptions.Start
+                    //Grid.SetColumn="5"
+                };
+
+            });
+        }
+
+        //public event ProgressChangedEventHandler PropertyChanged;
+        public Command FelixCommand
+        {
+            get;
+        }
+    }
+}
