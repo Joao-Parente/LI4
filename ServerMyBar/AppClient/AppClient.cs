05d7ca1f04c85e0632503806090f4f9d7b29b433
Gonçalo Pinto <61155765+GRP99@users.noreply.github.com>
2020-03-13 12:40:57 +0000

    tarefas...

--- ServerMyBar/AppClient/AppClient.cs
+++ ServerMyBar/AppClient/AppClient.cs
@@ -4,110 +4,73 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-namespace AppClient
-{
-    public class AppClient
-    {
 
-            
+namespace AppClient{
+    public class AppClient{
 
-                private static Socket master;
+        private static Socket master;
+        static void Main(string[] args){
+            int input;
+            byte[] msg = new byte[4];
+            bool flag = true;
+            LN ln = new LN();
 
-                static void Main(string[] args)
-                {
-                
-                        int input;
-                        byte[] msg= new byte[4];
-                        bool flag = true;
-                        
-                        
-                        LN ln= new LN();
-                        
-                        
-                        while (flag)
-                        {
-            
-                            
-                            // 1 Ver Produtos
-                            // 2 Pedidos Anteriores
-                            // 3 Alterar Pedido
-                            // 4 Efetuar Pedido <
-                            // 5 NoUlitmoPedido
-                            // 6 NovoProdutoFavorito
-                            // 7 InfoEmpresa
-                            // 8 AvaliarProduto
-                            // 9 IniciarSessao  <
-                            // 10 TerminarSessao
-                            // 11 RegistarNovoCliente
-                            // 12 Reclamacao
+            while (flag){
+                // 1 Ver Produtos
+                // 2 Pedidos Anteriores
+                // 3 Alterar Pedido
+                // 4 Efetuar Pedido <
+                // 5 NoUlitmoPedido
+                // 6 NovoProdutoFavorito
+                // 7 InfoEmpresa
+                // 8 AvaliarProduto
+                // 9 IniciarSessao  <
+                // 10 TerminarSessao
+                // 11 RegistarNovoCliente
+                // 12 Reclamacao
 
-                            Console.WriteLine("Insira: \n 4 para Fazer um pedido \n 5 para numeroultimopedido \n 9 para Fazer login  \n 11 para Registar");
-                            input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Insira: \n 4 para Fazer um pedido \n 5 para NoUlitmoPedido \n 9 para Fazer login  \n 11 para Registar");
+                input = Convert.ToInt32(Console.ReadLine());
 
-                            switch (input)
-                            {
-                                case 4: //Novo_Pedido
-                                    
-
-                                    ln.EfetuarPedido(new Pedido());
-                                    
-                                    break;
-
-                                case 9: // Login
-                                    
-                                    Console.WriteLine("Starting authentication");
-                                    
-                                    //Parse email e password
-                                    Console.WriteLine("Insira email");
-                                    string email = Console.ReadLine();
-                                    Console.WriteLine("Insira pw");
-                                    string pw = Console.ReadLine();
-
-                            
-
-                                    ln.IniciarSessao(email, pw);
-                                    
-                                    break;
-                                
-                                case 5: // NoUlitmoPedido
-
-
-                                    List<int> numeros = ln.NoUltimoPedido();
-                                  
-                                    break;
-                                default:
-                                    flag = false;
-                                    break;
-                                
-                                case 11 : //RegistarCliente
-
-                                    
-                                    //Parse email e password
-                                    Console.WriteLine("Insira email");
-                                    string e = Console.ReadLine();
-                                    Console.WriteLine("Insira pw");
-                                    string p = Console.ReadLine();
-                                    Console.WriteLine("Insira nome");
-                                    string n = Console.ReadLine();
-                                    
-                                    ln.RegistaUtilizador(e, p,n);
-                                    
-                                    break;
-                                    
-                                
-                             
-                            }
-                            
-                            msg= new byte[100];
-                        }
-
-             
-
+                switch (input){
+                    case 1: //VerProdutos
+                        ln.verProdutos();
+                        break;
+                    case 4: //Novo_Pedido
+                        ln.EfetuarPedido(new Pedido());
+                        break;
+                    case 5: // NoUlitmoPedido
+                        List<int> numeros = ln.NoUltimoPedido();
+                        break;
+                    case 9: // Login
+                        Console.WriteLine("Starting authentication");
+                        //Parse email e password
+                        Console.WriteLine("Insira email");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Insira pw");
+                        string pw = Console.ReadLine();
+                        ln.IniciarSessao(email, pw);
+                        break;
+                    case 10: //Logout
+                        ln.TerminarSessao();
+                        break;
+                    case 11: //RegistarCliente
+                        //Parse email e password
+                        Console.WriteLine("Insira email");
+                        string e = Console.ReadLine();
+                        Console.WriteLine("Insira pw");
+                        string p = Console.ReadLine();
+                        Console.WriteLine("Insira nome");
+                        string n = Console.ReadLine();
+                        ln.RegistaUtilizador(e, p, n);
+                        break;
+                    default:
+                        flag = false;
+                        break;
                 }
-                
-
-        
-                
+                msg = new byte[100];
+            }
+        }
     }
 }
-    
+
--- ServerMyBar/AppClient/LN.cs
+++ ServerMyBar/AppClient/LN.cs
@@ -34,24 +34,24 @@ namespace AppClient
         }
 
 
-        public Boolean RegistaUtilizador(string email, string password,string nome)
+        public Boolean RegistaUtilizador(string email, string password, string nome)
         {
             //envia id operacao
             byte[] id = new byte[4];
-            id=BitConverter.GetBytes(11); 
-            master.Send(id);  
-           
-           
-            byte [] msg= new byte[512];
-            string email_pw_nome = email + "|" + password +"|" + nome+ "|" ;
+            id = BitConverter.GetBytes(11);
+            master.Send(id);
+
+
+            byte[] msg = new byte[512];
+            string email_pw_nome = email + "|" + password + "|" + nome + "|";
             msg = Encoding.ASCII.GetBytes(email_pw_nome);
             master.Send(msg);
-        
-                            
-            byte [] log= new byte[30];
+
+
+            byte[] log = new byte[30];
             master.Receive(log);
-            bool login =BitConverter.ToBoolean(log, 0);
-                                    
+            bool login = BitConverter.ToBoolean(log, 0);
+
             if (login) Console.WriteLine("Sign me in");
             else Console.WriteLine("Dont mess with the system");
 
@@ -59,49 +59,60 @@ namespace AppClient
             return login;
         }
 
-        // +VerProdutos() : Lista Produtos 
+        public Dictionary<string,List<Produto>> verProdutos()
+        {
+            Dictionary<string, List<Produto>> dic = new Dictionary<string, List<Produto>>();
+            byte[] id = new byte[4];
+            id = BitConverter.GetBytes(1);
+            master.Send(id);
+
+
+
+            return dic;
+        }
 
         // +VerPedidosAnteriores() : Lista Pedidos
 
         // +AlterarPedido(idPedido : int, produtos : Lista Produtos) : boolean
 
-       public List<int> NoUltimoPedido() {
-            
-            List<int> r= new List<int>();
-            
-           //envia id operacao
-           byte[] id = new byte[4];
-           id=BitConverter.GetBytes(5); 
-           master.Send(id);
- 
-            byte [] num= new byte[4];
-            master.Receive(num,4,SocketFlags.None);
+        public List<int> NoUltimoPedido()
+        {
+
+            List<int> r = new List<int>();
+
+            //envia id operacao
+            byte[] id = new byte[4];
+            id = BitConverter.GetBytes(5);
+            master.Send(id);
+
+            byte[] num = new byte[4];
+            master.Receive(num, 4, SocketFlags.None);
             int x = BitConverter.ToInt32(num);
-            
-            
-            num= new byte[4];
-            master.Receive(num,4,SocketFlags.None);
+
+
+            num = new byte[4];
+            master.Receive(num, 4, SocketFlags.None);
             int y = BitConverter.ToInt32(num);
-            
-            r.Add(x);r.Add(y);                        
-            
-            Console.WriteLine("Numero ultimo pedido"+x + "e o outro "+y);
+
+            r.Add(x); r.Add(y);
+
+            Console.WriteLine("Numero ultimo pedido" + x + "e o outro " + y);
 
 
             return r;
-       }
+        }
 
 
-    //+AdicionarAosFavoritos(idProduto : int) : void
+        //+AdicionarAosFavoritos(idProduto : int) : void
 
 
         public int EfetuarPedido(Pedido p)
-        { 
-            
+        {
+
             //envia id operacao
-           byte[] id = new byte[4];
-           id=BitConverter.GetBytes(4); 
-           master.Send(id);
+            byte[] id = new byte[4];
+            id = BitConverter.GetBytes(4);
+            master.Send(id);
 
             enviaPedido(p);
             return 1;
@@ -113,48 +124,55 @@ namespace AppClient
         //+AvaliarProduto(idProduto : int, idCliente : int, nota : int) : void
 
 
-       public void IniciarSessao(string email, string password) {
-        
-           //envia id operacao
-           byte[] id = new byte[4];
-           id=BitConverter.GetBytes(9); 
-           master.Send(id);  
-           
-           
-           byte [] msg= new byte[512];
-           string email_pw = email + "|" + password +"|";
-           msg = Encoding.ASCII.GetBytes(email_pw);
-           master.Send(msg);
-        
-                            
-           byte [] log= new byte[30];
-           master.Receive(log);
-           bool login =BitConverter.ToBoolean(log, 0);
-                                    
-           if (login) Console.WriteLine("i'm in you crazy bastard");
-           else Console.WriteLine("we will get em next time");
-           
-    }
+        public void IniciarSessao(string email, string password)
+        {
+
+            //envia id operacao
+            byte[] id = new byte[4];
+            id = BitConverter.GetBytes(9);
+            master.Send(id);
 
 
-    //+TerminarSessao()
+            byte[] msg = new byte[512];
+            string email_pw = email + "|" + password + "|";
+            msg = Encoding.ASCII.GetBytes(email_pw);
+            master.Send(msg);
+
+
+            byte[] log = new byte[30];
+            master.Receive(log);
+            bool login = BitConverter.ToBoolean(log, 0);
+
+            if (login) Console.WriteLine("i'm in you crazy bastard");
+            else Console.WriteLine("we will get em next time");
+
+        }
+
+
+        public void TerminarSessao()
+        {
+            byte[] id = new byte[4];
+            id = BitConverter.GetBytes(10);
+            master.Send(id);
+            
+        }
 
         //+reclamacao(idCliente : int, idPedido : int, comentario : string) : boolean
 
-    
-    
-    
-    
-    
-    public  void enviaPedido (Pedido p)
-    {
-    byte[] pedido = p.SavetoBytes();
 
-    master.Send(BitConverter.GetBytes(pedido.Length)); // envia numero bytes    
-    master.Send(pedido);
-                    
-    } 
-    
-}
-    
+
+
+
+
+        public void enviaPedido(Pedido p)
+        {
+            byte[] pedido = p.SavetoBytes();
+
+            master.Send(BitConverter.GetBytes(pedido.Length)); // envia numero bytes    
+            master.Send(pedido);
+
+        }
+
+    }
+
 }
