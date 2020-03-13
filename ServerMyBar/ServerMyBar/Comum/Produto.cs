85d6dc70d07a3f300e86c1454490d719ebf524cf
Gonçalo Pinto <61155765+GRP99@users.noreply.github.com>
2020-03-13 12:52:14 +0000

    Add files via upload

--- ServerMyBar/ServerMyBar/Comum/Produto.cs
+++ ServerMyBar/ServerMyBar/Comum/Produto.cs
@@ -1,31 +1,41 @@
 namespace ServerMyBar.comum
 {
 
-        public class Produto
-        {
-            public int id { get; set; }
-            public string nome { get; set; }
-            public float preco { get; set; }
-            public string tags { get; set; }
-            public string imagem { get; set; }
-            
-            public Produto()
-            {
-                id = 1;
-                nome = "Arroz com atum";
-                preco = 3.5f ;
-                tags = "glutenfree vegan";
-                imagem = "muitos bytes";
+    public class Produto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public float preco { get; set; }
+        public string tags { get; set; }
+        public int disponibilidade { get; set; }
+        public string imagem { get; set; }
 
-            }
+        public Produto()
+        {
+            id = 1;
+            nome = "Arroz com atum";
+            preco = 3.5f;
+            tags = "glutenfree vegan";
+            imagem = "muitos bytes";
+
+        }
+
+        public Produto(int i, string n, string d, int di, float p)
+        {
+            id = i;
+            nome = n;
+            preco = p;
+            tags = d;
+            disponibilidade = di;
+        }
 
             public override string ToString()
-            {
-                return ("Id: " + id +
-                        "\n Nome: " + nome +
-                        "\n Preço: " + preco +
-                        "\n Tags: " + tags +
-                        "\n Imagem: " + imagem);
-            }
+        {
+            return ("Id: " + id +
+                    "\n Nome: " + nome +
+                    "\n Preço: " + preco +
+                    "\n Tags: " + tags +
+                    "\n Imagem: " + imagem);
         }
     }
+}
--- ServerMyBar/ServerMyBar/Comum/ProdutoDAO.cs
+++ ServerMyBar/ServerMyBar/Comum/ProdutoDAO.cs
@@ -1,7 +1,65 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
 namespace ServerMyBar.comum
 {
     public class ProdutoDAO
     {
         // +getProduto(id : int) : Produto
+
+        public static Dictionary<string, List<Produto>> VerTodos()
+        {
+            Dictionary<string, List<Produto>> dic = new Dictionary<string, List<Produto>>();
+            MySqlConnection conn;
+            string myConnectionString;
+            myConnectionString = @"server=127.0.0.1;uid=root;" +
+                                 "pwd=password;database=LI_Database";
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+
+                string query = "SELECT * FROM LI_Database.Produto;";
+                cmd.CommandText = query;
+                cmd.Prepare();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Produto p = new Produto((int) reader.GetInt64(0), (string) reader.GetString(2), (string) reader.GetString(3), (int) reader.GetUInt64(4), (float) reader.GetFloat(5));
+                        if (dic.ContainsKey(reader.GetString(1)))
+                        {
+                            List<Produto> lp = new List<Produto>();
+                            dic.TryGetValue(reader.GetString(1), out lp);
+                            lp.Add(p);
+                            dic.Add(reader.GetString(1), lp);
+                        }
+                        else
+                        {
+                            List<Produto> lp = new List<Produto>();
+                            lp.Add(p);
+                            dic.Add(reader.GetString(1), lp);
+                        }
+                    }
+                    return dic;
+                }
+                else
+                {
+                    Console.WriteLine(" !!no rows found.!!");
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Exception " + ex.Message);
+            }
+            return null;
+        }
     }
 }
