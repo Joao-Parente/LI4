1856142ed07e3a77bbd801d56b759732641560f2
jnuno420 <61151080+jnuno420@users.noreply.github.com>
2020-03-26 20:44:48 +0000

    Criada uma thread para simular a entrada de novos pedidos

---
+++ AppFuncionario/AppFuncionario/AppFuncionario/ThreadNovosPedidos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+namespace AppFuncionario
+{
+    public class ThreadNovosPedidos
+    {
+        public ListView listView { get; set; }
+        public ObservableCollection<Pedido> pds { get; set; }
+
+        public ThreadNovosPedidos(ListView v, ObservableCollection<Pedido> p)
+        {
+            this.listView = v;
+            this.pds = p;
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                //List<Pedido> ret = new List<Pedido>();
+                Random r = new Random();
+                List<Produto> produtos = new List<Produto>();
+                produtos.Add(new Produto("" + r.Next(100) + "hello", (float)r.NextDouble()));
+                produtos.Add(new Produto("" + r.Next(350) + "fromotherside", (float)r.NextDouble()));
+                pds.Add(new Pedido(1,DateTime.Now.ToString(),"sem atuna"+ r.Next(100),DateTime.Now,produtos));
+                pds.Add(new Pedido(1, DateTime.Now.ToString(), "sem atuna" + r.Next(100), DateTime.Now, produtos));/*
+                pds.Add(new Pedido(1, DateTime.Now.ToString(), "sem atuna" + r.Next(100), DateTime.Now, produtos));
+                pds.Add(new Pedido(1, DateTime.Now.ToString(), "sem atuna" + r.Next(100), DateTime.Now, produtos));
+                ret.Add(new Pedido(1, DateTime.Now.ToString(), "sem atuna" + r.Next(100), DateTime.Now, produtos));
+                ret.Add(new Pedido(1, DateTime.Now.ToString(), "sem atuna" + r.Next(100), DateTime.Now, produtos));
+                ret.Add(new Pedido(1, DateTime.Now.ToString(), "sem atuna" + r.Next(100), DateTime.Now, produtos));*/
+                /*
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    //listView.ItemsSource = null;
+                    listView.ItemsSource = ret;
+                    //listView.BeginRefresh();
+                });*/
+
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
